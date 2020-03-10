121759cecd62dd48696913abb57ad6f6c7f8739c
Carlos Oliveira <carlos.oliveira@codebyte.com.br>
2020-03-10 14:18:41 -0300

    create project

---
+++ Cadastro10Pessoas/Cadastro10Pessoas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cadastro10Pessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pessoa = new Pessoa();
+            var listaDeCadastros = new List<Pessoa>();
+            var possoRegistrar = true;
+            do
+            {
+                Console.WriteLine("Digite o nome da pessoa: ");
+                pessoa.Nome = Console.ReadLine();
+                if (string.IsNullOrEmpty(pessoa.Nome))
+                {
+                    Console.WriteLine("O nome da pessoa não pode ser vazio! ");
+                }
+                else
+                {
+                    if (listaDeCadastros.Count > 10)
+                    {
+                        possoRegistrar = false;
+                        Console.WriteLine("Erro! Limite máximo de pessoas atingido!");
+                    }
+                    else
+                    {
+                        listaDeCadastros.Add(pessoa);
+                    }
+                }
+
+            } while (possoRegistrar);
+        }
+    }
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+            CadastradoEm = DateTime.Now;
+        }
+        public string Nome { get; set; }
+        public DateTime CadastradoEm { get; set; }
+    }
+}
